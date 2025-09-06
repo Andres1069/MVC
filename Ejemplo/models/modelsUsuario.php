2aaf4a7f49d5da7a40cadf3c5e5ed8682af14bdb
Andres1069 <raulandresgonzalezcifuentes@gmail.com>
2025-09-06 10:17:56 -0500

    Clase 6/09/2025

---
+++ Ejemplo/models/modelsUsuario.php
@@ -0,0 +1,49 @@
+<?php
+
+class Usuario{
+    public $nombre;
+    public $apellidos;
+    public $email;
+    public $password;
+
+
+    function getNombre(){
+        return $this->nombre;
+    }
+
+        function getApellidos(){
+        return $this->apellidos;
+    }
+
+    function getEmail(){
+        return $this->email;
+    }
+
+    function getPassword(){
+        return $this->password;
+    }
+
+    function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    function setApellidos($apellidos){
+        $this->apellidos = $apellidos;
+    }
+
+    function setEmail($email){
+        $this->email = $email;
+    }
+
+    function setPassword($password){
+        $this->password = $password;
+    }
+
+
+    //metodo que consulte la base de datos 
+
+    public function conseguirtodos(){
+        echo 'Imprimiendo todos los usuarios ...';
+    }
+
+}
